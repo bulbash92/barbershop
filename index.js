95d82879984229bce58102da3c69c35a4c6b74a6
Evgeniy <49danon49@gmail.com>
2019-11-17 21:07:04 +0300

    animations styles for popup

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 var link = document.querySelector(".login-link");
 var popup = document.querySelector(".modal-login");
+var close = document.querySelector(".modal-close");
+var login = popup.querySelector("[name=login]");
+var password = popup.querySelector("[name=password]");
+var form = popup.querySelector("form");
+var storage = localStorage.getItem("login");
+
+
+form.addEventListener("submit", function(evt){
+    if (!login.value || !password.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+    } else {
+        localStorage.setItem("login", login.value);
+    }
+});
+
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-})
+   
+    if (storage) {
+        login.value = storage;
+        password.focus();
+    } else {
+        login.focus();
+    }
+});
 
-var close = document.querySelector(".modal-close");
+
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error")
+});
+
+window.addEventListener("keydown", function(evt){
+    if(evt.keyCode === 27) {
+        if (popup.classList.contains("modal-show")){
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error")
+        }
+    }
 })
+
